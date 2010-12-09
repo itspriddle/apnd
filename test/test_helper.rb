89ecc73e3595931b76a536f7b3ca365ebc29d2ce
Joshua Priddle <jpriddle@nevercraft.net>
2010-12-09 00:34:59 -0500

    Clean up test helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,33 +1,10 @@
 require 'rubygems'
 require 'test/unit'
-
-dir = File.dirname(File.expand_path(__FILE__))
-$LOAD_PATH.unshift(File.join(dir, '..', 'lib'))
-$LOAD_PATH.unshift(dir)
-
-require 'apnd'
+require 'shoulda'
 
 begin
   require 'turn'
 rescue LoadError
 end
 
-##
-# test/spec/mini 3
-# http://gist.github.com/25455
-# chris@ozmm.org
-#
-def context(*args, &block)
-  return super unless (name = args.first) && block
-  require 'test/unit'
-  klass = Class.new(defined?(ActiveSupport::TestCase) ? ActiveSupport::TestCase : Test::Unit::TestCase) do
-    def self.test(name, &block)
-      define_method("test_#{name.gsub(/\W/,'_')}", &block) if block
-    end
-    def self.xtest(*args) end
-    def self.setup(&block) define_method(:setup, &block) end
-    def self.teardown(&block) define_method(:teardown, &block) end
-  end
-  (class << klass; self end).send(:define_method, :name) { name.gsub(/\W/,'_') }
-  klass.class_eval &block
-end
+require 'apnd'
