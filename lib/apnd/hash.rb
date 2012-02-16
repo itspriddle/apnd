a2ba7d1dc42b9ad1f04bb68eba0c215221687aa9
Joshua Priddle <jpriddle@me.com>
2012-02-16 00:45:25 -0500

    Be more specific

--- lib/apnd/hash.rb
+++ lib/apnd/hash.rb
@@ -1,7 +1,9 @@
 class Hash
+  # Public: Returns a copy of Hash with all keys set as symbols. If values are
+  # Hashes, they are traversed and converted as well.
   def deep_symbolize
     inject({}) do |hash, (key, val)|
-      val = val.deep_symbolize if val.is_a?(Hash)
+      val = val.deep_symbolize if val.respond_to?(:deep_symbolize)
       hash[key.to_sym] = val
       hash
     end
